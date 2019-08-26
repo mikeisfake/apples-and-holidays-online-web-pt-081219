d4fa5e17ddee3dd4072bb3f4310793da10e35395
Mike Cooper <mikescooper@icloud.com>
2019-08-26 00:07:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,8 +48,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holidays|
     holidays.select do |holiday, supplies|
-      
-    end
+      supplies.include?("BBQ")
+    end.keys
   end 
 end
 
