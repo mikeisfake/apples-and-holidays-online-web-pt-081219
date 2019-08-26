5337943d7cdae8d72d88d1e51f752484f2816ee7
Mike Cooper <mikescooper@icloud.com>
2019-08-26 00:09:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,9 +40,7 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    return season.to_s
-  end 
+  
 end
 
 def all_holidays_with_bbq(holiday_hash)
